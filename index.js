4fe92db5f66a3aed702f163668574ac2d032218a
secthor01 <emersondirectx@gmail.com>
2023-10-24 12:51:25 -0300

    atualização

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log("Hello Wordão!!")
+let nomeHeroi = "Carlão"
+let xp = 3674;
+
+
+
+while(xp <= 1000){
+    console.log(`O Herói de nome ${nomeHeroi} está no nível FERRO`)
+    break
+    
+}    
+if(xp >= 1001 & xp <= 2000){
+    console.log(`O Herói de nome ${nomeHeroi} está no nível BRONZE`)
+    
+}
+
+if(xp >= 2001 & xp <= 5000){
+    console.log(`O Herói de nome ${nomeHeroi} está no nível PRATA`)
+}
+if(xp >= 6001 & xp <= 7000){
+    console.log(`O Herói de nome ${nomeHeroi} está no nível OURO`)
+}
+if(xp >= 7001 & xp <= 8000 ){
+    console.log(`O Herói de nome ${nomeHeroi} está no nível PLATINA`)
+}
+if(xp >= 8001 & xp <= 9000){
+    console.log(`O Herói de nome ${nomeHeroi} está no nível ASCENDENTE`)
+}
+if(xp >= 9001 & xp <= 10000){
+    console.log(`O Herói de nome ${nomeHeroi} está no nível IMORTAL`)
+}
+if(xp >= 10001){
+    console.log(`O Herói de nome ${nomeHeroi} está no nível RADIANTE`)
+}
+
+
+
+
+    
+
+
+
